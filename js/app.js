f7e9ce88b8a2aa4ae4e30df3db501965cf76a643
Rincevent42 <porvooc@gmail.com>
2020-03-27 12:35:48 +0100

    Dark mode : forms done.

--- js/app.js
+++ js/app.js
@@ -1,34 +1,30 @@
- /* Text animation in the header */
+/* Text animation in the header */
 
 document.addEventListener('DOMContentLoaded', function () {
-    const planetes = ["Arrakis", "Pandora", "Coruscant","Mars"];
-    const change = document.getElementById('changeText');
-    let i = 0;
-    const intervalId = setInterval(function () {
-        change.innerHTML = planetes[i];
-        change.classList.add("slide")
-        i++;
-        if (i > 3) {
-            i = 0;
-        }
-    }, 3000)
+  const planetes = ['Arrakis', 'Pandora', 'Coruscant', 'Mars'];
+  const change = document.getElementById('changeText');
+  let i = 0;
+  const intervalId = setInterval(function () {
+    change.innerHTML = planetes[i];
+    change.classList.add('slide');
+    i++;
+    if (i > 3) {
+      i = 0;
+    }
+  }, 3000)
 })
 
-
-
 /* Dark mode / light mode toggleling */
-function switchTheme(event) {
-    if (event.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        //setCookie('darkMode',true,1);
-        localStorage.setItem('darkMode','dark');
-        console.log('Dark mode set');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        //setCookie('darkMode',false,1);
-        localStorage.setItem('darkMode','light');
-        console.log('Dark mode unset');
-    }    
+function switchTheme (event) {
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('darkMode','dark');
+    console.log('Dark mode set');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('darkMode','light');
+    console.log('Dark mode unset');
+  }
 }
 
 const toggler = document.querySelector('#toggler');
@@ -39,16 +35,10 @@ according to the value of the local storage variable 'darkMode' */
 const darkMode = localStorage.getItem('darkMode');
 console.log('Dark mode Local Storage variable set to : ' + darkMode);
 document.documentElement.setAttribute('data-theme', darkMode);
-if (darkMode == 'dark') {
-    document.getElementById("toggler").checked = true;
-    console.log("Dark mode checkbox status changed to checked");
+if (darkMode === 'dark') {
+  document.getElementById('toggler').checked = true;
+  console.log('Dark mode checkbox status changed to checked');
 } else {
-    document.getElementById("toggler").checked = false;
-    console.log("Dark mode checkbox status changed to unchecked");
+  document.getElementById('toggler').checked = false;
+  console.log('Dark mode checkbox status changed to unchecked');
 }
-
-
-
-
-
- 
